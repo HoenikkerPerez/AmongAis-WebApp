34e955749e44dc57621ca2d4bd033e256e85b93f
Michele <meek@localhost.localdomain>
2020-11-06 19:51:01 +0100

    Barely nothing in practice

--- clients/game-client/game-client.js
+++ clients/game-client/game-client.js
@@ -1,1 +1,1 @@
-export default class GameClient extends TotallyUnreliableGameClient {}
+class GameClient {}
--- clients/game-client/lobby-manager.js
+++ clients/game-client/lobby-manager.js
@@ -1,4 +1,4 @@
-export default class LobbyManager {    
+/*export default*/ class LobbyManager {    
 
     constructor(client) {}
 
