53baf87260f029dfb2e75b4da7ad1c128e846b52
Edoardo Maione <edoardo.maione@gmail.com>
2020-11-08 18:19:58 +0100

    first scatch of login

---
+++ model/model.js
@@ -0,0 +1,6 @@
+var model = {
+    login = false,
+    
+    getLogin:  function() {return this.login},
+    setLogin: function(lg) {this.login=lg}
+}
--- view/ui.js
+++ view/ui.js
@@ -1,1 +1,6 @@
-// prende  
+export default function login(){
+    // var form = document.getElementById("form");
+    var username = document.login_form.username;
+    // console.log(username);
+    window.alert(username);
+}
