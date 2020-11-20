ea9145c98f07fcdbfc51b51d0374849ef220dc9d
Luca Peretti <l.peretti@studenti.unipi.it>
2020-11-20 10:44:59 +0100

    popupmsg on connection errors

--- clients/chat-client.js
+++ clients/chat-client.js
@@ -12,7 +12,11 @@ class ChatClient {
         this._ws = new WebSocket(model.net.chat.ws, ['binary','base64']);
         this._ws.onopen = function(evt) { console.debug("Chat Client opened the WebSocket.") };
         this._ws.onclose = function(evt) { console.debug("Chat Client closed the connection.") };
-        this._ws.onerror = function(evt) { console.error("Chat Client error: " + evt.data) };
+        this._ws.onerror = function(evt) { 
+            console.error("Chat Client error: " + evt.data);
+            popupMsg("Chat Server connection error!", "danger");
+
+        };
         // this._ws.onmessage = function(evt) { console.error("Chat Received: " + evt.data) };
     }
 
--- clients/game-client/game-client.js
+++ clients/game-client/game-client.js
@@ -33,7 +33,10 @@ class GameClient {
         this._ws = new WebSocket(model.net.game.ws, ['binary','base64']);
         this._ws.onopen = function(evt) { console.debug("Game Client opened the WebSocket.") };
         this._ws.onclose = function(evt) { console.debug("Game Client closed the connection.") };
-        this._ws.onerror = function(evt) { console.error("Game Client error: " + evt.data) };
+        this._ws.onerror = function(evt) { 
+            popupMsg("Game Server connection error!", "danger");
+            console.error("Game Client error: " + evt); 
+        };
 
         this._wsQueue = [];
         this._ws.onmessage = async function(evt) {
