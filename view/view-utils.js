a285ecb9bb5539519bc456dac846de56bf77a33f
Edoardo Maione <edoardo.maione@gmail.com>
2020-11-30 22:26:26 +0100

    stacked notification

--- view/view-utils.js
+++ view/view-utils.js
@@ -1,10 +1,15 @@
 // "alert-popup"
 // "alert-popup-msg"
 popupMsg = function(msg, kind){ // kind: success / info / warning / danger
-    let content = document.getElementById("alert-popup-msg");
+    let d = new Date();
+    let id = "popup-msg-" + d.getTime();
+    $("#alert-popup").append("<div id=\""+id+"\" class=\"modal-dialog\"><div class=\"alert alert-warning\" style=\"white-space: pre;\"><strong>Warning!</strong> Indicates a warning that might need attention.</div></div>");
+    // alert_msg.className = 
+    let content = document.getElementById(id);
+    content = content.childNodes[0]
     content.textContent=msg;
     content.className = "alert alert-"+kind;
-
-    $('#alert-popup').modal('show');
     
+    $('#alert-popup').modal('show');
+    window.setTimeout(function(){ $("#"+id).remove() }.bind(this), 10000);
 };
