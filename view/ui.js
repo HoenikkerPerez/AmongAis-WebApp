40618ac89a5dab26ed9b58ad5b19d587d695435a
Luca Peretti <l.peretti@studenti.unipi.it>
2020-12-15 15:12:08 +0100

    better looking score results

--- view/ui.js
+++ view/ui.js
@@ -43,21 +43,74 @@ class Ui {
         document.getElementById("endgame-title").innerText = message;
     }
 
+    // _refreshLadder() {
+    //     let endgameDiv = document.getElementById("endgame-ladder");
+    //     endgameDiv.innerHTML = '';
+    //     for(let endscore of model.endgameScore) {
+    //         let el = document.createElement("h4");
+    //         let teamColor = endscore.team == "0" ? "red" : "blue";
+    //         el.innerText = endscore.score + " / " + endscore.name;
+    //         el.style.color = teamColor;
+    //         endgameDiv.appendChild(el);
+    //     }
+    //     endgameDiv = document.getElementById("endgame");
+    //     endgameDiv.style.display="";
+    //     //left-hud
+    //     let leftHUD = document.getElementById("left-hud");
+    //     leftHUD.style.display="none";
+    // }
+
     _refreshLadder() {
-        let endgameDiv = document.getElementById("endgame-ladder");
-        endgameDiv.innerHTML = '';
-        for(let endscore of model.endgameScore) {
-            let el = document.createElement("h4");
-            let teamColor = endscore.team == "0" ? "red" : "blue";
-            el.innerText = endscore.score + " / " + endscore.name;
-            el.style.color = teamColor;
-            endgameDiv.appendChild(el);
-        }
-        endgameDiv = document.getElementById("endgame");
-        endgameDiv.style.display="";
-        //left-hud
-        let leftHUD = document.getElementById("left-hud");
-        leftHUD.style.display="none";
+        let tournamentModalLabel = document.getElementById("resultsModalLabel");
+        tournamentModalLabel.innerHTML = "SCORES"
+
+        let tournamentModalBody = document.getElementById("resultsModalBody");
+        tournamentModalBody.innerHTML = '';
+
+        let resultsTable = document.createElement("table")
+        resultsTable.classList.add("table");
+        
+        let tr = document.createElement("tr");
+
+        let t1= document.createElement("th");
+        t1.innerHTML = "Name";
+
+        let t2 = document.createElement("th");
+        t2.innerHTML = "Score";
+
+        let t3 = document.createElement("th");
+        t3.innerHTML = "Team";
+
+        tr.appendChild(t1);
+        tr.appendChild(t2);
+        tr.appendChild(t3);
+
+        resultsTable.appendChild(tr)
+
+        model.endgameScore.forEach(endscore => {
+            let tEl = document.createElement("tr");
+
+            let usernameEl = document.createElement("td");
+            usernameEl.innerHTML = endscore.name;
+            
+            let scoreEl = document.createElement("td");
+            scoreEl.innerHTML = endscore.score;
+            
+            let teamEl = document.createElement("td");
+            // teamEl.innerHTML = endscore.team;
+            let dot = document.createElement("span")
+            let teamClass = endscore.team == "0" ? "redDot" : "blueDot";
+            dot.classList.add(teamClass);
+            teamEl.appendChild(dot)
+
+            tEl.appendChild(usernameEl);
+            tEl.appendChild(scoreEl);
+            tEl.appendChild(teamEl);
+            
+            resultsTable.appendChild(tEl);   
+        });
+        tournamentModalBody.appendChild(resultsTable);
+        $('#resultsModal').modal('toggle');
     }
 
     _loadWsMessages() {
@@ -90,6 +143,8 @@ class Ui {
             let msg = evt.detail.message;
             this._gameEnded(msg);
             
+            // open modal results
+            // $('#resultsModal').modal('toggle');
         }, false);
 
         document.addEventListener("MODEL_ENDGAME_SCORE_ADDED", () => {
