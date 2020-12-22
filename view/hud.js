b46909665449044324ecbb15e2669a954eac49c5
Luca Peretti <l.peretti@studenti.unipi.it>
2020-12-22 13:16:39 +0100

    added law settings and exit icons

--- view/hud.js
+++ view/hud.js
@@ -259,15 +259,27 @@ class HudUi {
                 if(model.status.me != {} && model.status.me.team == _p.team && model.status.me.symbol != _p.symbol) { // && model.status.me.symbol != _p.symbol
                     // Social deduction
                     let accuse_button = document.createElement("button");
-                    accuse_button.innerText = "Accuse!";
-                    accuse_button.classList.add("accuse-button");
+                    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                    svg.setAttribute("xmlns","http://www.w3.org/2000/svg")
+                    svg.setAttribute("viewBox","0 0 16 16")
+                    svg.setAttribute("width","16")
+                    svg.setAttribute("height","16")
+                    var path = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
+                    path.setAttribute("fill-rule","evenodd"); //Set path's data fill-rule="evenodd"
+                    path.setAttribute("d","M8.75.75a.75.75 0 00-1.5 0V2h-.984c-.305 0-.604.08-.869.23l-1.288.737A.25.25 0 013.984 3H1.75a.75.75 0 000 1.5h.428L.066 9.192a.75.75 0 00.154.838l.53-.53-.53.53v.001l.002.002.002.002.006.006.016.015.045.04a3.514 3.514 0 00.686.45A4.492 4.492 0 003 11c.88 0 1.556-.22 2.023-.454a3.515 3.515 0 00.686-.45l.045-.04.016-.015.006-.006.002-.002.001-.002L5.25 9.5l.53.53a.75.75 0 00.154-.838L3.822 4.5h.162c.305 0 .604-.08.869-.23l1.289-.737a.25.25 0 01.124-.033h.984V13h-2.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-2.5V3.5h.984a.25.25 0 01.124.033l1.29.736c.264.152.563.231.868.231h.162l-2.112 4.692a.75.75 0 00.154.838l.53-.53-.53.53v.001l.002.002.002.002.006.006.016.015.045.04a3.517 3.517 0 00.686.45A4.492 4.492 0 0013 11c.88 0 1.556-.22 2.023-.454a3.512 3.512 0 00.686-.45l.045-.04.01-.01.006-.005.006-.006.002-.002.001-.002-.529-.531.53.53a.75.75 0 00.154-.838L13.823 4.5h.427a.75.75 0 000-1.5h-2.234a.25.25 0 01-.124-.033l-1.29-.736A1.75 1.75 0 009.735 2H8.75V.75zM1.695 9.227c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L3 6.327l-1.305 2.9zm10 0c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L13 6.327l-1.305 2.9z"); //Set path's data fill-rule="evenodd"
+                    // newElement.style.stroke = "#000"; //Set stroke colour
+                    // newElement.style.strokeWidth = "5px"; //Set stroke width
+                    svg.appendChild(path);
+                    accuse_button.appendChild(svg)
+
+                    // accuse_button.classList.add("accuse-button");
                     accuse_button.classList.add("btn");
                     accuse_button.classList.add("btn-danger");
                     accuse_button.classList.add("btn-sm");
                     accuse_button.classList.add("px-1");
                     accuse_button.classList.add("py-0");
                     accuse_button.classList.add("ml-1");
-
+                    
                     accuse_button.onclick= () => {
                         document.dispatchEvent(new CustomEvent("BUTTON_ACCUSE", {detail: _p.name }));
                         let allb = document.getElementsByClassName("accuse-button");
