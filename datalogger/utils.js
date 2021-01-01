5d403108963acffedfc8c3864f1db6ce512952a5
Edoardo Maione <edoardo.maione@gmail.com>
2021-01-01 18:19:46 +0100

    add some typecheck

--- datalogger/utils.js
+++ datalogger/utils.js
@@ -7,7 +7,13 @@ let _datalogSample = {
 
 let logData = function (datalog, urlTargetPHP="datalogger/home.php") {
     console.debug("logData");
-
+    if( typeof(datalog)!="object" ||
+        typeof(datalog.username)!="string" ||
+        typeof(datalog.success)!="number"
+        ){
+            console.error("WRONG DATALOG!");
+            return;
+    }
     $.ajax({
         url: urlTargetPHP,
         error: function(xhr, status, error) {
