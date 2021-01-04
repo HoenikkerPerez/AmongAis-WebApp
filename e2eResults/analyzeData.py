dfdd21dcecdacfdc9948387311a899d8befc8d3f
Luca Peretti <l.peretti@studenti.unipi.it>
2021-01-04 19:31:40 +0100

    map settings plot added

--- e2eResults/analyzeData.py
+++ e2eResults/analyzeData.py
@@ -56,27 +56,67 @@ req_time_match = df_match_op[["session_id", "req_time"]]
 req_times = req_time_home.append(req_time_match)
 req_times_grouped = req_times.groupby(by="session_id", as_index=False)
 print(req_times)
-print("----------------------------------")
+
+# print("----------------------------------")
+size_sums = df_map_settings_op.groupby(by="size")["size"].count().sort_values(ascending=False)
+# print(size_sums.head())
+labels = size_sums.keys().tolist()
+sizes = size_sums.to_numpy()
+explode = (0, 0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=False, startangle=90)
+ax1.set(aspect="equal", title='Map Size')
+plt.show()
+# print("----------------------------------")
+
+balanced_sums = df_map_settings_op.groupby(by="balanced")["balanced"].count().sort_values(ascending=False)
+labels = ["Balanced" if x==1 else "Unbalanced" for x in balanced_sums.keys().tolist()]
+sizes = balanced_sums.to_numpy()
+explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=False, startangle=90)
+ax1.set(aspect="equal", title='Map Balanced')
+plt.show()
+# print("----------------------------------")
+
+square_sums = df_map_settings_op.groupby(by="square")["square"].count().sort_values(ascending=False)
+labels = ["Square" if x==1 else "Rectangular" for x in square_sums.keys().tolist()]
+sizes = square_sums.to_numpy()
+explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=False, startangle=90)
+ax1.set(aspect="equal", title='Map Type')
+plt.show()
+
+bos_sums = df_map_settings_op.groupby(by="bos")["bos"].count().sort_values(ascending=False)
+labels = ["Battle of Species" if x==1 else "Normal" for x in bos_sums.keys().tolist()]
+sizes = square_sums.to_numpy()
+explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=False, startangle=90)
+ax1.set(aspect="equal", title='Map Battle of species')
+plt.show()
 print("----------------------------------")
 
-# print(df_map_settings_op.describe())
-settings = df_map_settings_op[["size", "balanced","square","bos"]]
-size_sums = df_map_settings_op.groupby(by="size")["size"].count()
-print(size_sums.head())
-print("----------------------------------")
 
-balanced_sums = df_map_settings_op.groupby(by="balanced")["balanced"].count()
-print(balanced_sums.head())
-print("----------------------------------")
+# most frequent set of settings used to create matches
+print(df_settings_op.head())
+# sums = df_map_settings_op.groupby(by="bos")["bos"].count().sort_values(ascending=False)
+# labels = ["Battle of Species" if x==1 else "Normal" for x in bos_sums.keys().tolist()]
+# sizes = square_sums.to_numpy()
+# explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+# fig1, ax1 = plt.subplots()
+# ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+#         shadow=False, startangle=90)
+# ax1.set(aspect="equal", title='Map Battle of species')
+# plt.show()
 
-square_sums = df_map_settings_op.groupby(by="square")["square"].count()
-print(square_sums.head())
-print("----------------------------------")
 
-bos_sums = df_map_settings_op.groupby(by="bos")["bos"].count()
-print(bos_sums.head())
-print("----------------------------------")
-
+#
 # size_sums["count"] = sizes.
 # print(settings_avg)
 # print(df_home_op.dtypes)
